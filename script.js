163f28753ec9112b6cb6b4f99c3584919da7bba4
AtimaB <nickzehn10@hotmail.com>
2021-02-20 17:32:08 -0700

    submit btn is working

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-function ssnLength(elem){
-    
-    if (elem.value.length = 4) {
-        elem.value = elem.value.slice(0,4); 
-    } 
-}
- 
-
 const dob = document.getElementById('inputDate');
 const ssn = document.getElementById('last4');
 const form = document.getElementById('form');
-const errorElement  = document.getElementById('error')
 
-form.addEventListener('.submitBtn', (e) => {
-    let messages = []
-    if (ssn.value === '' || ssnLength(elem) == null) {
-        messages.push('Plese enter the last 4 SSN')
+form.addEventListener('input', ()=> {
+    if (ssn.value.length > 3 && dob.value.length > 0)  {
+        submitBtn.removeAttribute('disabled'); 
+     
+    } else {
+        submitBtn.setAttribure('disable', 'disable');
     }
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
-    }
+} );
 
-})
+
+function showAlert() {
+    alert ("Successfully Submitted");
+  }
+
 
